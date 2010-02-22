4a2d2f566d2cea627a362740c8ddc5628200dda6
Alex Pooley <alex@brownbeagle.com.au>
2010-02-22 14:08:44 +0800

    cleaning up codez

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,41 +1,39 @@
 class Post
   POSTS_PATH = "#{RAILS_ROOT}/app/views/posts"
 
-  attr_reader :file, :id, :title, :updated_at, :created_at, :post_meta
+  attr_reader :file, :id, :title, :updated_at, :created_at, :meta
 
   def initialize(path)
-    # Fetch meta data for blog post.
-    @post_meta = meta[@id]
-
     # The path of the source file.
     @file = path
     # views/posts/post_first_post.html.haml => first_post
-    name = File.basename(file).split('.').first[5..-1]
     # The id of this post for url_for.
-    @id = name
+    @id = File.basename(file).split('.').first[5..-1]
+
+    # Fetch meta data for blog post.
+    @meta = self.class.blog_meta[@id]
 
     # Create a default set of meta values for this post.
-    @post_meta[@id] ||= {}
-    @post_meta[@id].reverse_merge!({
+    @meta ||= {}
+    @meta.reverse_merge!({
       'title' => nil,
       'created_at' => nil,
       'updated_at' => nil
     })
 
     # The title of the post.
-    @title = @post_meta[@id]['title'] || name.humanize
+    @title = @meta['title'] || @id.humanize
     # When this post created and updated.
-    @updated_at = @post_meta[@id]['updated_at'] || File::mtime(file)
-    @created_at = @post_meta[@id]['created_at'] || @updated_at
+    @updated_at = @meta['updated_at'] || File::mtime(file)
+    @created_at = @meta['created_at'] || @updated_at
   end
 
   # Return meta data for blog.
-  def self.meta
-    m = if File.exists?(POSTS_PATH+'/meta.yml')
+  def self.blog_meta
+    m = {}
+    if File.exists?(POSTS_PATH+'/meta.yml')
       parsed_yaml = ERB.new(IO.read(POSTS_PATH+'/meta.yml')).result
-      YAML::load(parsed_yaml)
-    else
-      {}
+      m = YAML::load(parsed_yaml)
     end
     return m
   end
